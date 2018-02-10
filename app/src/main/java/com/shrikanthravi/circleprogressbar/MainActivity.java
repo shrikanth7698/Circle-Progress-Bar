866a1e93a6334ed0d4e808afd7d558ca520b0887
shrikanth7698 <shrikanth7698@gmail.com>
2018-02-10 19:33:45 +0530

    Completed Circle progressbar and added stickman animation

--- app/src/main/java/com/shrikanthravi/circleprogressbar/MainActivity.java
+++ app/src/main/java/com/shrikanthravi/circleprogressbar/MainActivity.java
@@ -1,13 +1,21 @@
 package com.shrikanthravi.circleprogressbar;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView stickImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AnimationDrawable animationDrawable = ((AnimationDrawable) getResources().getDrawable(R.drawable.stick_animation));
+        stickImageView = findViewById(R.id.stickImageView);
+        stickImageView.setBackground(animationDrawable);
+        animationDrawable.start();
     }
 }
